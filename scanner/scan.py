9a85543ea9f5b356a76b8101d6fd35889e5a99b7
Orb_H <orb.cov@gmail.com>
2019-06-22 00:29:06 +0900

    scan.py added / batch file added

--- scanner/scan.py
+++ scanner/scan.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class Scanner:
 
     def __init__(self):
@@ -6,9 +9,16 @@ class Scanner:
         self.keywords = ['IF', 'THEN', 'ELSE', 'WHILE']
         self.digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
         self.letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        self.whitespaces = [' ', '\n', '\t', '\r']
         self.tokens = []
+    
+    def scan_file(self, f):  # Run this function once to read all the inputs. Just call get_tokens at the end to get all the tokens from input.
+        f = open(sys.argv[1], 'r')
+        lines = f.readlines()
+        for line in lines:
+            self.scan_string(line)
 
-    def scan(self, s):
+    def scan_string(self, s):  # Run this function as many as you want. Just call get_tokens at the end to get all the tokens from input.
         while len(s) != 0:
             if self.check_separator(s):
                 self.tokens.append(('separator', s[0]))
@@ -24,42 +34,44 @@ class Scanner:
                 else:
                     a = self.check_digit(s)
                     if a > 0:
-                        print('num ' + s[0:a])
+                        # print('num ' + s[0:a])
                         self.tokens.append(('num', s[0:a]))
                         s = s[a:]
                     else:
                         a = self.check_letter(s)
                         if a > 0:
-                            print('word ' + s[0:a])
+                            # print('word ' + s[0:a])
                             self.tokens.append(('word', s[0:a]))
                             s = s[a:]
+                        elif s[0] in self.whitespaces:
+                            s = s[1:]
                         else:
                             print('Lexer Error: Unknown Symbol')
     
     def check_separator(self, s):
         if s[0] in self.separators:
-            print('separator ' + s[0])
+            # print('separator ' + s[0])
             return True
         return False
 
     def check_operator(self, s):
         if s[0] in self.operators:
-            print('operator ' + s[0])
+            # print('operator ' + s[0])
             return True
         return False
 
     def check_keyword(self, s):
         if (len(s) == 2 and s[0:2] == self.keywords[0]) or (len(s) > 2 and s[0:2] == self.keywords[0] and s[2] not in self.letters):
-            print('keyword IF')
+            # print('keyword IF')
             return 2
         elif (len(s) == 4 and s[0:4] == self.keywords[1]) or (len(s) > 4 and s[0:4] == self.keywords[1] and s[4] not in self.letters):
-            print('keyword THEN')
+            # print('keyword THEN')
             return 4
         elif (len(s) == 4 and s[0:4] == self.keywords[2]) or (len(s) > 4 and s[0:4] == self.keywords[2] and s[4] not in self.letters):
-            print('keyword ELSE')
+            # print('keyword ELSE')
             return 4
         elif (len(s) == 5 and s[0:5] == self.keywords[3]) or (len(s) > 5 and s[0:5] == self.keywords[3] and s[5] not in self.letters):
-            print('keyword WHILE')
+            # print('keyword WHILE')
             return 5
         return 0
 
@@ -109,9 +121,14 @@ class SymbolTable:
 
 if __name__ == "__main__":  # main for TEST
     scan = Scanner()
-    while True:
-        s = input()
-        if s == '.':
-            break
-        scan.scan(s)
-    print(scan.get_tokens())
+    if len(sys.argv) == 1:
+        while True:
+            s = input()
+            if s == '.':
+                break
+            scan.scan(s)
+        print(scan.get_tokens())
+    elif len(sys.argv) == 2:
+        scan.scan_file(open(sys.argv[1], 'r'))
+        print(scan.get_tokens())
+        
