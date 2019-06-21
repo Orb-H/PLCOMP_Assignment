baa5313c86a523c364391071c974c163e7f7498e
Orb_H <orb.cov@gmail.com>
2019-06-22 01:17:39 +0900

    symbol table attached to scanner

--- scanner/scan.py
+++ scanner/scan.py
@@ -11,6 +11,7 @@ class Scanner:
         self.letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
         self.whitespaces = [' ', '\n', '\t', '\r']
         self.tokens = []
+        self.table = SymbolTable()
     
     def scan_file(self, f):  # Run this function once to read all the inputs. Just call get_tokens at the end to get all the tokens from input.
         f = open(sys.argv[1], 'r')
@@ -57,6 +58,8 @@ class Scanner:
     def check_operator(self, s):
         if s[0] in self.operators:
             # print('operator ' + s[0])
+            if s[0] == '=':
+                self.table.add_symbol(self.tokens[-1][1])
             return True
         return False
 
@@ -96,6 +99,9 @@ class Scanner:
     def get_tokens(self):
         return self.tokens
 
+    def get_symbol_table(self):
+        return self.table
+
 
 class SymbolTable:
 
@@ -103,21 +109,38 @@ class SymbolTable:
         self.d = {}
     
     def add_symbol(self, c):
-        self.d[c] = [None, None]
+        if self.find_symbol(c) == None:
+            self.d[c] = [None, None]
     
     def find_symbol(self, c):
         return self.d.get(c)
+    
+    def get_scope(self, c):
+        if self.d.get(c) == None:
+            return None
+        return self.d[c][0]
     
     def set_scope(self, c, scope):
         if self.d.get(c) == None:
             raise Error("No such symbol in table")
         self.d[c][0] = scope
+        
+    def get_value(self, c):
+        if self.d.get(c) == None:
+            return None
+        return self.d[c][1]
     
     def set_value(self, c, value):
         if self.d.get(c) == None:
             raise Error("No such symbol in table")
         self.d[c][1] = value
-            
+        
+    def __str__(self):
+        s = 'Name\tScope\tValue\n'
+        for i in self.d.keys():
+            s += i + '\t' + str(self.get_scope(i)) + '\t' + str(self.get_value(i)) + '\n'
+        return s
+
 
 if __name__ == "__main__":  # main for TEST
     scan = Scanner()
@@ -126,9 +149,11 @@ if __name__ == "__main__":  # main for TEST
             s = input()
             if s == '.':
                 break
-            scan.scan(s)
+            scan.scan_string(s)
         print(scan.get_tokens())
+        print(scan.get_symbol_table())
     elif len(sys.argv) == 2:
         scan.scan_file(open(sys.argv[1], 'r'))
         print(scan.get_tokens())
+        print(scan.get_symbol_table())
         
